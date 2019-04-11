dc1ce7580929c99678f8336bfc34fec200ec0ea7
Kim Duclos <kimduclos@gmail.com>
2019-04-10 20:40:10 -0600

    built with failure

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,48 @@
 // Test away!
+import React from "react";
+import { render, cleanup } from "react-testing-library";
+import renderer from "react-test-renderer";
+import "jest-dom/extend-expect";
+
+import Controls from "./Controls";
+import Dashboard from "../dashboard/Dashboard";
+
+afterEach(cleanup);
+
+describe('Controls Tests', () => {
+  it('renders', () => {
+    render(<Controls />);
+  });
+
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Controls />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+
+  it('toggle correct buttons - trigger the right actions.', () => {
+    const { getByText } = render(<Dashboard />);
+
+    const closeButton = getByText(/Closed/);
+    const lockButton = getByText(/Locked/);
+
+    expect(lockButton).toBeDisabled();
+    
+    triggerEvent.click(closeButton);
+    getByText(/ClosedGate/);
+
+    triggerEvent.click(lockButton);
+    getByText(/LockedGate/);
+    
+    const openButton = getByText(/Open/);
+    
+    expect(openButton).toBeDisabled();
+    
+    const unlockButton = getByText(/Unlock/);
+    
+    triggerEvent.click(unlockButton);
+    getByText(/UnlockedGate/);
+    
+    triggerEvent.click(openButton);
+    getByText(/OpenGate/);
+  });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,61 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+import renderer from 'react-test-renderer';
+import 'jest-dom/extend-expect';
+import Display from './Display';
+import Dashboard from '../dashboard/Dashboard';
+
+afterEach(cleanup);
+
+describe('Display tests', () => {
+  it(renders, () => {
+    render(<Display />);
+  });
+
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Display />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+
+  it('correct colors and words on load', () => {
+    const { getByText } = render(<Display />);
+
+    const greenOpen = getByText(/Open/);
+    const greenUnlocked = getByText(/Unlocked/);
+
+    expect(greenOpen).toHaveClass('green-led');
+    expect(greenUnlocked).toHaveClass('green-led');
+  });
+
+  it('correct colors for event when triggered', () => {
+    const { getByText } = render(<Dashboard />);
+
+    const greenOpen = getByText(/Open/);
+    const greenUnlocked = getByText(/Unlocked/);
+
+    expect(greenOpen).toHaveClass('green-led');
+    expect(greenUnlocked).toHaveClass('green-led');
+    
+    const closeButton = getByText(/Close Gate/);
+    
+    // red for closed / locked
+    fireEvent.click(closeButton);
+    const redClosed = getByText(/Closed/);
+    expect(redClosed).toHaveClass('red-led');
+    const lockButton = getByText(/Lock Gate/);
+    
+    fireEvent.click(lockButton);
+    const redLocked = getByText(/Locked/);
+    expect(redLocked).toHaveClass('red-led');
+    const unlockButton = getByText(/Unlock Gate/);
+    
+    // green for open / unlocked
+    fireEvent.click(unlockButton);
+    expect(greenUnlocked).toHaveClass('green-led');
+    const openButton = getByText(/Open Gate/);
+    
+    fireEvent.click(openButton);
+    expect(greenOpen).toHaveClass('green-led');
+  });
+});
